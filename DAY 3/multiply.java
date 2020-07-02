dd6c57075da0eb0a3800d4ccdea093a4063de62c
Visels <elviskipchumba@yahoo.com>
2020-07-02 17:18:11 +0300

    Added files for the first two days

---
+++ DAY 3/multiply.java
@@ -0,0 +1,34 @@
+public class multiply{
+
+
+public static void main(String[] args){
+
+int number1 = 10;
+int number2 = 20;
+
+int result = number1 * number2;
+
+
+
+//printing out the product of the two
+
+System.out.println("The product is ##" + product(number1, number2));
+System.out.println("The sum is ##" + sum(number1, number2));
+}
+
+
+//product methond or function
+static int product (int a, int b){
+
+	return a*b;
+}
+
+//sum method or function
+static int sum(int a, int b){
+	return a+b;
+}
+
+
+
+
+}
