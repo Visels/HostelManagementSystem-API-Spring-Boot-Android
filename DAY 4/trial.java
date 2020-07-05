8f396f1e521f8840e5cb125e1d09fc3aa3c814a5
Visels <elviskipchumba@yahoo.com>
2020-07-05 20:11:18 +0300

    added trial files

---
+++ DAY 4/trial.java
@@ -0,0 +1,20 @@
+public class trial{
+
+public static void main(String args[]){
+    int[] numbers;
+
+
+	System.out.println("This is a trial printed line.");
+
+
+	for (int i = 0; i<10;i++){
+
+
+		numbers[i] = i;
+
+
+	}
+}
+
+
+}
